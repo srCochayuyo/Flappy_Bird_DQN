78b88b8024a258181db09958afd3b641a697e79b
srCochayuyo <129439145+srCochayuyo@users.noreply.github.com>
2025-06-29 18:09:55 -0400

    Doc: Add doc of actions and rewars

--- Main.py
+++ Main.py
@@ -2,17 +2,48 @@
 import gymnasium as gym
 import flappy_bird_gymnasium
 
+
+"""
+|ACCIONES|
+|
+|0 --> No hace nada|
+|1 --> Salta/Vuela |
+
+---------------------------------------------------------------
+
+|RECOMEPENSAS|
+|               
+| +0.1 --> Cada frame que se mantiene vivo|
+| +1.0 --> Cada tuberia pasada
+| -1.0 --> Cada vez que muere  
+| -0.5 --> Cada vez que toca la parte superior de la pantalla
+
+"""
+
+
+
 env = gym.make("FlappyBird-v0", render_mode="human", use_lidar=True)
 
-obs, _ = env.reset()
+state, _ = env.reset()
+total_reward = 0
+steps = 0
+episode = 0
+
 while True:
 
     action = env.action_space.sample()
 
-    obs, reward, terminated, _, info = env.step(action)
+    state, reward, terminated, _, info = env.step(action)
+
+    total_reward += reward
+    episode += 1
+    steps += 1
+
+    print("Episode:",episode," Total Reward:",total_reward," Steps:",steps)
     
 
     if terminated:
         break
+
 
 env.close()
